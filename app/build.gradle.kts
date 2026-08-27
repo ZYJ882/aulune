118031147e2206b3727ecc1cc4d0e042c7504230
ZYJ882 <1.09709327e+08+ZYJ882@users.noreply.github.com>
2026-08-27 11:08:14 +0000

    release: prepare Aulune v1.8.2 debug [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.aulune.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100000035
-        versionName = "1.8.1"
+        versionCode = 100000036
+        versionName = "1.8.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
